2a5ec32c5a96e61190f459b12c30e36eb462dd4b
Akhundzadafatima <165835698+Akhundzadafatima@users.noreply.github.com>
2024-10-22 00:07:01 +0200

    Add files via upload

---
+++ propsTest2/src/components/TheList.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul class="headerList">
+        <li>Home</li>
+        <li>Contact</li>
+        <li>Blog</li>
+        <li>
+            {{ Fatime }}
+        </li>
+
+        </ul>
+
+   
+</template>
+<script>
+
+export default{
+
+components:{
+},
+
+props: {
+    Fatime:{
+        type: String,
+        default:'heseennnn',
+        Required:true
+
+    }
+  }
+
+
+}
+</script>
---
+++ propsTest2/src/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    <img
+      class="logo"
+      src="https://static.vecteezy.com/system/resources/previews/008/296/267/non_2x/colorful-swirl-logo-design-concept-illustration-vector.jpg"
+      alt=""
+    />
+    <list  :Fatime="fatime5vurma"/>
+    <input type="text" v-model="number">
+  </div>
+</template> 
+
+<script>
+import list from "./TheList.vue";
+export default {
+  data(){
+    return{
+      number:null,
+    }
+  },
+  components: {
+    list,
+  },
+  computed:{
+  fatime5vurma(){
+   return this.number*5
+  }, 
+}
+//   fatime5bolme(){
+//    return this.test/5
+//   }
+// }
+};
+
+</script>
